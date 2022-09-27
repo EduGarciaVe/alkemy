9b6f2fb544652ae93b43e27f967b793f4d383823
Eduardo Garcia Venegas <edugarcia.venegas@gmail.com>
2022-09-26 21:44:36 -0300

    Entidad Genero

---
+++ src/main/java/com/alkemy/disney/model/Genero.java
@@ -0,0 +1,30 @@
+package com.alkemy.disney.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Data
+
+@Entity
+public class Genero {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	@Lob
+	@Column(name = "fileimage", columnDefinition = "BLOB")
+	private byte[] fileImage;
+	
+}
